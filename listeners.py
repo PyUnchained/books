4c8862a329ffd9dfe409ff5d6c03fcd398d56c2e
Tatenda. K. Tambo <tatendatambo@gmail.com>
2019-10-16 18:56:42 +0200

    Initial commit after 18 Upgrade.

--- listeners.py
+++ listeners.py
@@ -6,12 +6,6 @@ from django.dispatch import receiver
 from django.db import transaction
 from django.db.models import Q
 
-
-from management.models import (Booking, RegisteredAddress, Consignment,Parcel, CompletionCode,
-    ATCommand, BroadcastEvent, Offer, Driver, Client)
-from management.models.signals import*
-from management.dj_channels.consumers.gsm_gateway import send_text_cmd
-
 from books.signals.api_signals import double_entry_sig,create_account_sig
 from books.tasks import create_account
 
